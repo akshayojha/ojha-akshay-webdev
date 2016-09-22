c4eae70f510946b5d08ab031595f2b8aee92a323
Akshay Ojha <akshayojha@users.noreply.github.com>
2016-09-22 10:06:15 -0400

    More heroku changes

--- test/app.js
+++ test/app.js
@@ -6,14 +6,6 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
-    }
-
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
